bb2a49badb9411bf775321f8eca7eb39317e0a65
Wilkerson <24ewilkerson@FVFZMXV7J1WK.tld>
2022-03-16 11:26:05 -0500

    added more code

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 alert("Welcome to 20! This is a competition to see which player gets closer to the number 20. You are going to be playing against the computer. \n You will draw numbers from 1 to 10. The computer will stop at 16. Pay attention to the screen to see what your total is, and if you need another number, press 'y'.");
+
+alert("Now it is computer's turn");
+
+computertotal = 0;
+
+do {
+  var computernumber = Math.floor(Math.random() * 10) + 1
+  computertotal = computertotal + computernumber
+  alert(`Computer drew ${computernumber} \n Computer total is ${computertotal}`)
+} while (computertotal < 16)
+
+alert(`Computer will stop at ${computertotal}`)
+
+usertotal = 0;
+
+var usernumber = Math.floor(Math.random() * 10) + 1
+
+usertotal = usertotal + usernumber
+
+alert(`User drew ${usernumber} \n User total is ${usertotal}`)
+
+var userquestion = prompt("Would you like to draw again? Type 'y' for yes. Any other option will stop the draw."); 
+
+if (userquestion == "y") {
+  
+}
+
+alert(`User will stop at ${usertotal}`)
