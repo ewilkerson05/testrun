58c8f90f9f26bc914a9383730cbb2684d9f65022
Wilkerson <24ewilkerson@FVFZMXV7J1WK.tld>
2022-03-22 11:57:18 -0500

    finished twenty game

--- app.js
+++ app.js
@@ -2,27 +2,71 @@ alert("Welcome to 20! This is a competition to see which player gets closer to t
 
 alert("Now it is computer's turn");
 
-computertotal = 0;
-
-do {
-  var computernumber = Math.floor(Math.random() * 10) + 1
-  computertotal = computertotal + computernumber
-  alert(`Computer drew ${computernumber} \n Computer total is ${computertotal}`);
-} while (computertotal < 16)
-
-alert(`Computer will stop at ${computertotal}`)
-
-usertotal = 0;
+computerMatch = 0;
+userMatch = 0;
 
 do {
 
-  var usernumber = Math.floor(Math.random() * 10) + 1
+  computertotal = 0;
 
-  usertotal = usertotal + usernumber
+  do {
 
-  var userquestion = prompt(`User drew ${usernumber} \n User total is ${usertotal}.\n\n Would you like to draw again? Type 'y' for yes. Any other option will stop the draw.`); 
+    var computernumber = Math.floor(Math.random() * 10) + 1
 
-} while ((userquestion == "y") && (usertotal < 20))
+    computertotal = computertotal + computernumber
 
-alert(`User will stop at ${usertotal}`);
+    alert(`Computer drew ${computernumber} \n Computer total is ${computertotal}`);
 
+  } while (computertotal < 16)
+
+  alert(`Computer will stop at ${computertotal}`)
+
+  usertotal = 0;
+
+  do {
+
+    var usernumber = Math.floor(Math.random() * 10) + 1
+
+    usertotal = usertotal + usernumber
+
+    var userquestion = prompt(`User drew ${usernumber} \n User total is ${usertotal}.\n\n Would you like to draw again? Type 'y' for yes. Any other option will stop the draw.`); 
+
+  } while ((userquestion == "y") && (usertotal < 20))
+
+  alert(`User will stop at ${usertotal}`);
+
+  if ((usertotal <= 20) && (computertotal > 20)) {
+
+    alert(`User won this round.`)
+    userMatch++
+
+  } else if ((usertotal == 20) && (computertotal != 20)) {
+
+    alert(`User won this round.`)
+    userMatch++
+    
+  } else if ((usertotal > 20) && (computertotal <= 20)) {
+
+    alert(`Computer won this round.`)
+    computerMatch++
+    
+  } else if ((usertotal < 20) && (computertotal == 20)) {
+
+    alert(`Computer won this round.`)
+    computerMatch++
+    
+  } else if ((usertotal > 20) && (computertotal > 20)) {
+
+    alert(`No one won this round.`)
+    
+  } else if ((usertotal == 20) && (computertotal == 20)) {
+
+    alert(`It's a tie.`)
+    
+  }
+
+  var playagain = prompt(`Current score is\n\nComouter: ${computerMatch}\n\nUser: ${userMatch}\n\nWould you like to play again? Type 'y' for yes. Any other option will exit the game.`);
+
+} while (playagain == "y")
+
+alert(`Final Score:\n\nComputer: ${computerMatch}\n\nUser: ${userMatch}.`)
